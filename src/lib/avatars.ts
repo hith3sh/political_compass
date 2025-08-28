ef664a60b96c8efefe0311a654305793f8139a7f
hith3sh <hithesh0215@gmail.com>
2025-08-28 21:11:30 +0530

    nalin avatar

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -22,7 +22,7 @@ export const availableAvatars: Avatar[] = [
   { id: 'mahinda', filename: 'mahinda.jpeg', name: 'Mahinda Rajapaksa' },
   { id: 'mathini', filename: 'mathini.jpg', name: 'Mathini' },
   { id: 'melani', filename: 'melani.jpeg', name: 'Melani Gunathilake' },
-  { id: 'nalin', filename: 'nalin.jpg', name: 'Nalin De Silva' },
+  { id: 'nalin', filename: 'nalin.jpeg', name: 'Nalin De Silva' },
   { id: 'nirmal', filename: 'nirmal_dewasiri.jpg', name: 'Nirmal Dewasiri' },
   { id: 'pubudu', filename: 'pubudu_jagoda.jpg', name: 'Pubudu Jagoda' },
   { id: 'ranil', filename: 'ranil.jpg', name: 'Ranil Wickremesinghe' },
@@ -34,7 +34,7 @@ export const availableAvatars: Avatar[] = [
   { id: 'thamalu', filename: 'thamalu.jpg', name: 'Thamalu Piyadigama' },
   { id: 'tilvin', filename: 'tilvin.jpg', name: 'Tilvin Perera' },
   { id: 'upali', filename: 'upali_kohomban.jpg', name: 'Upali Kohomban' },
-  { id: 'wangeesa', filename: 'wangeesa.jpeg', name: 'Wangeesa Sumanasekara' },
+  { id: 'wangeesa', filename: 'wangeesa.jpeg', name: 'Vangeesa Sumanasekara' },
   { id: 'wimal', filename: 'wimal_weerawansa.jpg', name: 'Wimal Weerawansa' }
 ];
 
